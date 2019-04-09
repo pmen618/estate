ec5a2003e1156947d1697491b0037a1a86d797c0
Pmen <pmen618@gmail.com>
2019-04-10 00:25:57 +0700

    add file into Estate project

---
+++ js/main.js
@@ -0,0 +1,61 @@
+(function($) {
+
+  new WOW().init();
+
+// Hide loading image
+  jQuery(window).load(function() {
+    jQuery('.doc-loader').fadeOut('slow');
+  });
+
+//Show accordion
+//
+$('.toggle').click(function(e) {
+    e.preventDefault();
+  
+    var $this = $(this);
+  
+    if ($this.next().hasClass('show')) {
+        $this.next().removeClass('show');
+        $this.next().slideUp(350);
+    } else {
+        $this.parent().parent().find('li .inner').removeClass('show');
+        $this.parent().parent().find('li .inner').slideUp(350);
+        $this.next().toggleClass('show');
+        $this.next().slideToggle(350);
+    }
+});
+// count number
+// 
+
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
+// Light box
+// 
+
+$(document).ready(function() {
+/* show lightbox when clicking a thumbnail */
+    $('a.thumb').click(function(event){
+      event.preventDefault();
+      var content = $('.modal-body');
+      content.empty();
+        var title = $(this).attr("title");
+        $('.modal-title').html(title);        
+        content.html($(this).html());
+        $(".modal-profile").modal({show:true});
+    });
+
+  });
+
+
+
+})(jQuery);
